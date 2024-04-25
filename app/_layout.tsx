8c02969f49e403089719d244da5aafac232a02cb
Marius STEPHANY <marius.stephany@gmail.com>
2024-04-25 23:02:18 +0200

    :wrench: Install NativeWind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -15,6 +15,8 @@ import { useEffect, useState } from "react";
 import "react-native-get-random-values";
 import { v4 as uuidv4 } from "uuid";
 
+import "../global.css";
+
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
