66d1df86a1b2cae6f8473e35cc3963d69034f4ed
Marius STEPHANY <marius.stephany@gmail.com>
2025-01-14 21:58:57 +0400

    :recycle: Remove useless namespace on socket path

--- utils/server.ts
+++ utils/server.ts
@@ -3,4 +3,4 @@ import { io } from "socket.io-client";
 export const apiUrl = process.env.EXPO_PUBLIC_API_URL;
 export const fetcher = (...args: any[]) =>
   fetch(...(args as [RequestInfo, RequestInit])).then((res) => res.json());
-export const socket = io(`${process.env.EXPO_PUBLIC_API_URL}/rooms`);
+export const socket = io(`${process.env.EXPO_PUBLIC_API_URL}`);
