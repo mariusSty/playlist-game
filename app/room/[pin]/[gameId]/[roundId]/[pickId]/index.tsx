574081388267bdd662d6a8fbdd8becb8bd3b0ff2
Marius STEPHANY <marius.stephany@gmail.com>
2025-01-17 14:45:54 +0400

    :sparkles: Can play preview

--- app/room/[pin]/[gameId]/[roundId]/[pickId]/index.tsx
+++ app/room/[pin]/[gameId]/[roundId]/[pickId]/index.tsx
@@ -3,6 +3,7 @@ import Container from "@/components/Container";
 import { UserContext } from "@/contexts/user-context";
 import { usePick, useRoom } from "@/hooks/useGame";
 import { socket } from "@/utils/server";
+import { Audio } from "expo-av";
 import { router, useLocalSearchParams } from "expo-router";
 import { useContext, useEffect, useState } from "react";
 import { Pressable, Text, View } from "react-native";
@@ -13,6 +14,7 @@ export default function Vote() {
   const { room } = useRoom(pin.toString());
   const { pick, isPickLoading } = usePick(pickId.toString());
   const [isVoteValidated, setIsVoteValidated] = useState(false);
+  const [sound, setSound] = useState<Audio.Sound | null>(null);
 
   function handleVote(guessId: string) {
     setIsVoteValidated(true);
@@ -29,6 +31,23 @@ export default function Vote() {
       pickId,
       userId: user.id,
     });
+  }
+
+  async function handlePlayPreview() {
+    if (pick) {
+      if (sound) {
+        await sound.unloadAsync();
+      }
+
+      await Audio.setAudioModeAsync({ playsInSilentModeIOS: true });
+      const { sound: newSound } = await Audio.Sound.createAsync(
+        { uri: pick.track.previewUrl },
+        { shouldPlay: true }
+      );
+
+      setSound(newSound);
+      await newSound.playAsync();
+    }
   }
 
   useEffect(() => {
@@ -49,22 +68,27 @@ export default function Vote() {
     };
   }, []);
 
+  useEffect(() => {
+    return () => {
+      if (sound) {
+        sound.unloadAsync();
+      }
+    };
+  }, [sound]);
+
   if (isPickLoading || !pick) {
     return <Text>Loading...</Text>;
   }
 
   return (
     <Container title="Listen and Vote !">
-      <Text className="text-white text-9xl">{pick.track.title}</Text>
+      <Button text="Play" onPress={handlePlayPreview} />
       {room?.users.map((player, index) => (
         <View
-          key={index}
+          key={player.id}
           className="flex-row items-center w-full gap-5 py-5 justify-evenly"
         >
-          <Text className="text-lg text-white">{index}</Text>
-          <Text className="text-lg text-white" key={index}>
-            {player.name}
-          </Text>
+          <Text className="text-lg text-white">{player.name}</Text>
           {!isVoteValidated && (
             <Pressable
               className="p-5 bg-white rounded-lg"
